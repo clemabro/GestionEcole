a7a364460b32015076be6cbccf7f3a4a7f4a6796
Antoine HOULBERT <houlbert.antoine@gmail.com>
2020-08-27 10:26:55 +0200

    Servlet connexion

---
+++ src/com/GestionEcole/servlet/ConnexionServlet.java
@@ -0,0 +1,15 @@
+package com.GestionEcole.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ConnexionServlet extends HttpServlet {
+
+	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+		
+	}
+}
