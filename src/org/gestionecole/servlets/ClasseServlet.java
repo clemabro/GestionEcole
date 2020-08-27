36339ac85b09e4c70813bbd413b11167ed189e36
clemabro <clementazibeiro@sfr.fr>
2020-08-27 15:02:06 +0200

    Accueil

---
+++ src/org/gestionecole/servlets/ClasseServlet.java
@@ -0,0 +1,20 @@
+package org.gestionecole.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ClasseServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1490023807697587876L;
+	
+	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher( "/WEB-INF/accueil.jsp").forward( request, response );
+	}
+}
